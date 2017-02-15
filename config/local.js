69caa791309fe745143b5ab6bbf9c956ca4310fb
Pratik Thakur <pratik.thakur@raweng.com>
2017-02-15 11:49:15 +0530

    first commit

---
+++ config/local.js
@@ -0,0 +1,15 @@
+module.exports = exports = {
+    port: "4000",
+    theme: "basic",
+    languages: [
+        {
+            "code": "en-us",
+            "relative_url_prefix": "/"
+        }
+    ],
+    plugins: {},
+    contentstack: {
+        api_key: "blt25d3ad4c8ff579fe",
+        access_token: "blt728e476668bd8cf1"
+    }
+};
